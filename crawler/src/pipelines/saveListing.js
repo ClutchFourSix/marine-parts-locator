86e3924e826b2733cee914cedd243d237315b9cb
ClutchFourSix <ericjfay@gmail.com>
2026-04-07 08:51:55 +0300

    Add save listing pipeline scaffold

---
+++ crawler/src/pipelines/saveListing.js
@@ -0,0 +1,4 @@
+export async function saveListing(listing) {
+  console.log('Save listing scaffold', listing?.title || 'unknown listing');
+  console.log('Next step: persist normalized listings to the database.');
+}
